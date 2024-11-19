0f9ebf5c13e137964d09f6020f7573e2ee505e2e
BauduccoT <tbauducco@gmail.com>
2024-11-19 19:39:47 -0300

    rutas protegidas funcionales
    navbar admin permite cerrar sesion e ir a login. no se puede acceder a login teniendo la sesion iniciada

--- src/componentes/comunAdmins/NavbarAdmins.jsx
+++ src/componentes/comunAdmins/NavbarAdmins.jsx
@@ -1,9 +1,16 @@
 import React, { useState } from 'react'
-import { Link } from 'wouter';
+import { Link, useLocation } from 'wouter';
 
-export default function NavbarAdmins() {
+export default function NavbarAdmins({actualizarToken}) {
 
   const [open, setOpen] = useState(false)
+  const [location, setLocation]=useLocation()
+
+  function cerrarSesion(){
+    sessionStorage.removeItem("token")
+    actualizarToken()
+    location("/login")
+  }
 
   return (
     <div className='bg-white py-3 fixed top-0 left-0 right-0 shadow-md'>
@@ -16,37 +23,49 @@ export default function NavbarAdmins() {
     <div className={`${!open && "hidden"} bg-gray-600/50 min-h-screen w-full fixed top-0 left-0 right-0 backdrop-blur-sm`} onClick={() => setOpen(false)}></div>
 
     <div className={`${open ? "w-80" : "w-0"} bg-cyan-600 min-h-screen fixed top-0 left-0 transition-all duration-300`}>
-      <div className={`${!open && "hidden"} pt-3`}>
+      <div className={`${!open && "hidden"} pt-3 h-screen`}>
         <button className='ml-4 text-white mb-14' onClick={() => setOpen(false)}>
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
             <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
           </svg>
         </button>
 
-          <Link to="/admin/productos">
-            <div className='text-center flex justify-between align-items text-white text-xl hover:bg-orange-400 cursor-pointer py-3 mb-2 p-24 '>Productos<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-          <path strokeLinecap="round" strokeLinejoin="round"  d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" /></svg>
-            </div>
-          </Link>
+        <Link to="/admin/productos">
+          <div className='text-center flex justify-between align-items text-white text-xl hover:bg-orange-400 cursor-pointer py-3 mb-2 p-24 '>Productos<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+            <path strokeLinecap="round" strokeLinejoin="round"  d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" /></svg>
+          </div>
+        </Link>
 
-          <Link to="/admin/categorias">
-            <div className='text-center  flex justify-between align-items text-white text-xl hover:bg-orange-400 cursor-pointer py-3 mb-2 p-24 '>Categorias<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-             <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 16.875h3.375m0 0h3.375m-3.375 0V13.5m0 3.375v3.375M6 10.5h2.25a2.25 2.25 0 0 0 2.25-2.25V6a2.25 2.25 0 0 0-2.25-2.25H6A2.25 2.25 0 0 0 3.75 6v2.25A2.25 2.25 0 0 0 6 10.5Zm0 9.75h2.25A2.25 2.25 0 0 0 10.5 18v-2.25a2.25 2.25 0 0 0-2.25-2.25H6a2.25 2.25 0 0 0-2.25 2.25V18A2.25 2.25 0 0 0 6 20.25Zm9.75-9.75H18a2.25 2.25 0 0 0 2.25-2.25V6A2.25 2.25 0 0 0 18 3.75h-2.25A2.25 2.25 0 0 0 13.5 6v2.25a2.25 2.25 0 0 0 2.25 2.25Z" /></svg>
-        </div>
-            
-          </Link>
+        <Link to="/admin/categorias">
+          <div className='text-center  flex justify-between align-items text-white text-xl hover:bg-orange-400 cursor-pointer py-3 mb-2 p-24 '>Categorias<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 16.875h3.375m0 0h3.375m-3.375 0V13.5m0 3.375v3.375M6 10.5h2.25a2.25 2.25 0 0 0 2.25-2.25V6a2.25 2.25 0 0 0-2.25-2.25H6A2.25 2.25 0 0 0 3.75 6v2.25A2.25 2.25 0 0 0 6 10.5Zm0 9.75h2.25A2.25 2.25 0 0 0 10.5 18v-2.25a2.25 2.25 0 0 0-2.25-2.25H6a2.25 2.25 0 0 0-2.25 2.25V18A2.25 2.25 0 0 0 6 20.25Zm9.75-9.75H18a2.25 2.25 0 0 0 2.25-2.25V6A2.25 2.25 0 0 0 18 3.75h-2.25A2.25 2.25 0 0 0 13.5 6v2.25a2.25 2.25 0 0 0 2.25 2.25Z" /></svg>
+          </div>
+          
+        </Link>
 
-          <Link to="/admin/admins">
-            <div className='text-center flex justify-between align-items text-white text-xl hover:bg-orange-400 cursor-pointer py-3 mb-2 p-24 '>Admins 
+        <Link to="/admin/admins">
+          <div className='text-center flex justify-between align-items text-white text-xl hover:bg-orange-400 cursor-pointer py-3 mb-2 p-24 '>Admins 
             <div className='flex flex-row  ' >
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6  ">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" /></svg>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6  ">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
+              </svg>
             </div>
-            </div>
+          </div>
 
-          </Link>
-       
+        </Link>
+        <div className={`overflow-hidden pt-3 flex absolute left-0 right-0 bottom-2 justify-center items-center text-white text-xl hover:bg-orange-400 py-2 mb-2 p-16`}>
+          <button className='flex flex-row gap-5' onClick={()=>cerrarSesion()}>
+            <p>
+              Cerrar Sesion
+            </p>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
+              <path fillRule="evenodd" d="M16.5 3.75a1.5 1.5 0 0 1 1.5 1.5v13.5a1.5 1.5 0 0 1-1.5 1.5h-6a1.5 1.5 0 0 1-1.5-1.5V15a.75.75 0 0 0-1.5 0v3.75a3 3 0 0 0 3 3h6a3 3 0 0 0 3-3V5.25a3 3 0 0 0-3-3h-6a3 3 0 0 0-3 3V9A.75.75 0 1 0 9 9V5.25a1.5 1.5 0 0 1 1.5-1.5h6ZM5.78 8.47a.75.75 0 0 0-1.06 0l-3 3a.75.75 0 0 0 0 1.06l3 3a.75.75 0 0 0 1.06-1.06l-1.72-1.72H15a.75.75 0 0 0 0-1.5H4.06l1.72-1.72a.75.75 0 0 0 0-1.06Z" clipRule="evenodd" />
+            </svg>
+          </button>
+        </div>
       </div>
+      
+
     </div>
   </div>
   );
